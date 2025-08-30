520d51212d30a285ace4dd50d909e60170d8e811
Koteimpulse <vladimir007700@gmail.com>
2025-08-30 23:00:30 +0300

    1

--- src/shared/utils/test-utils.ts
+++ src/shared/utils/test-utils.ts
@@ -4,12 +4,10 @@ import {execSync} from 'child_process';
 export const prisma = new PrismaClient();
 
 export const setupTestDatabase = async () => {
-	// для тестов можно просто подтянуть схему к тестовой БД
 	execSync('npx prisma db push', {stdio: 'inherit'});
 };
 
 export const teardownTestDatabase = async () => {
-	// чистим все таблицы после тестов
 	const tables = await prisma.$queryRaw<
 		Array<{tablename: string}>
 	>`SELECT tablename FROM pg_tables WHERE schemaname='public'`;
